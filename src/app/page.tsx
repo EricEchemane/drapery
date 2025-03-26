aac60a39eb3d5c5262de0d53c5f6ed564ef7c524
ericechemane <eechemane29@gmail.com>
2025-03-26 15:01:07 +0800

    small screen home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import HeroBannerSmallImage from '@/assets/home-page/banner-small.png';
 import HeroBannerImage from '@/assets/home-page/banner.png';
+import { cn } from '@/lib/utils';
+import { playfair } from '@/lib/constants';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <>
-      <section className="h-screen md:h-[50vh] lg:h-screen relative">
+      <section className="relative h-screen md:h-[50vh] lg:h-screen">
         <Image
           alt=""
           src={HeroBannerSmallImage}
@@ -20,7 +23,17 @@ export default function Home() {
           quality={100}
           className="object-cover absolute top-0 right-0 bottom-0 left-0 hidden md:inline"
         />
-        <div className="mt-32 p-6 absolute top-0 left-0 right-0"></div>
+        <div className="mt-32 p-6 absolute top-0 left-0 right-0 py-6 text-white">
+          <h1 className={cn('text-4xl mb-6', playfair.className)}>
+            Elegant Custom Drapery for Every Home
+          </h1>
+          <p className="mb-6 text-lg">
+            Premium quality | Made-to-measure | Professionally installed
+          </p>
+          <Button variant={'outline'} size={'lg'} className="backdrop-blur-md">
+            Get a quote from us now!
+          </Button>
+        </div>
       </section>
     </>
   );
--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -4,13 +4,15 @@ import MobileNav from './mobile-nav';
 
 export default function Navigation() {
   return (
-    <nav className="p-6 bg-black/30 z-10 absolute top-0 left-0 right-0 flex items-center">
-      <DraperyLogo className="w-[40px] lg:w-[50px]" />
+    <nav className="bg-gradient-to-b from-black/40 to-transparent z-10 absolute top-0 left-0 right-0">
+      <div className="flex items-center max-w-7xl mx-auto p-6">
+        <DraperyLogo className="w-[40px] lg:w-[55px]" />
 
-      <LargeScreenNav />
+        <LargeScreenNav />
 
-      <div className="ml-auto md:hidden">
-        <MobileNav />
+        <div className="ml-auto md:hidden">
+          <MobileNav />
+        </div>
       </div>
     </nav>
   );
