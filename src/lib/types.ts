9333bd93633e3b01427586a2e72d12f228ba4bb7
ericechemane <eechemane29@gmail.com>
2025-03-26 20:29:50 +0800

    customer reviews done

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,12 @@
+import { StaticImageData } from 'next/image';
+
 export interface Navlink {
   displayText: string;
   href: string;
+}
+
+export interface CustomerReview {
+  message: string;
+  avatar: StaticImageData;
+  name: string;
 }
