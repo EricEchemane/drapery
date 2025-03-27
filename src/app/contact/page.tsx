3309074e35aa57c23e3ac5f5d48497741ed5ac04
ericechemane <eechemane29@gmail.com>
2025-03-27 14:24:44 +0800

    okay

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,6 +7,7 @@ import { TextField } from '@mui/material';
 import { Button } from '@/components/ui/button';
 import { MapPin } from 'lucide-react';
 import { Separator } from '@/components/ui/separator';
+import { TransformYourSpace } from '../page';
 
 export const metadata: Metadata = {
   title: 'Contact',
@@ -14,98 +15,104 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <section className="relative min-h-[150vh] flex pt-36 justify-center px-10 -z-9">
-      <ContactHeroBackground />
-      <div className="items-center">
-        <div className="relative z-10 w-full max-w-6xl grid grid-cols-1 lg:grid-cols-[1fr_auto_1fr] gap-8 text-white">
-          <div className="order-3 lg:order-1 space-y-6 md:pr-6">
-            <div className="mb-14">
-              <h2 className={cn('text-3xl font-bold', playfair.className)}>
-                Follow us
-              </h2>
-              <div className="flex flex-row gap-6 mt-8">
-                <div>
-                  <p className="text-sm text-white/80">Facebook</p>
-                  <p className="font-medium text-xs md:text-sm">
-                    /The-Drapery-Studio/
-                  </p>
-                </div>
-                <div>
-                  <p className="text-sm text-white/80">Instagram</p>
-                  <p className="font-medium text-xs md:text-sm">
-                    /the_draperystudio
-                  </p>
+    <>
+      <section className="relative pt-36 lg:pb-24">
+        <ContactHeroBackground />
+        <div>
+          <div className="relative max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-[1fr_auto_1fr] gap-8 text-white">
+            <div className="order-3 lg:order-1 space-y-6">
+              <div className="mb-14 px-6">
+                <h2 className={cn('text-3xl font-bold', playfair.className)}>
+                  Follow us
+                </h2>
+                <div className="flex flex-row gap-6 mt-8">
+                  <div>
+                    <p className="text-sm text-white/80">Facebook</p>
+                    <p className="font-medium text-xs md:text-sm">
+                      /The-Drapery-Studio
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-sm text-white/80">Instagram</p>
+                    <p className="font-medium text-xs md:text-sm">
+                      /the_draperystudio
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <Separator className="block lg:hidden bg-white/40 h-[1px] w-full my-8" />
 
-            <div>
-              <h2 className={cn('text-3xl font-bold', playfair.className)}>
-                We’re here
-              </h2>
-              <div className="flex gap-2 mt-8 mb-10">
-                <span>
-                  <MapPin className="w-4 h-4" />
-                </span>
-                <span className="text-xs md:text-sm">
-                  SP&A Building 2, Xavierville Avenue, Quezon City, Philippines
-                </span>
+              <Separator className="block lg:hidden bg-white/40 h-[1px] w-full my-8" />
+
+              <div className="px-6">
+                <h2 className={cn('text-3xl font-bold', playfair.className)}>
+                  {"We're here"}
+                </h2>
+                <div className="flex gap-2 mt-8 mb-10">
+                  <span>
+                    <MapPin className="w-4 h-4" />
+                  </span>
+                  <span className="text-xs md:text-sm">
+                    SP&A Building 2, Xavierville Avenue, Quezon City,
+                    Philippines
+                  </span>
+                </div>
               </div>
+
+              <iframe
+                title="map"
+                src="https://maps.google.com/maps?q=Xavierville%20Ave%2C%20Quezon%20City&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                // className="w-[100vw]  lg:w-3/4 lg:ml-0 lg:rounded-xl h-[60vh] border-0"
+                className="w-full h-[60vh] lg:w-3/4 lg:rounded-xl border-0 lg:ml-6"
+                loading="eager"
+              />
             </div>
-            <iframe
-              title="map"
-              src="https://maps.google.com/maps?q=Xavierville%20Ave%2C%20Quezon%20City&t=&z=13&ie=UTF8&iwloc=&output=embed"
-              className="w-[100vw] -ml-10 lg:w-3/4 lg:ml-0 lg:rounded-xl h-[60vh] border-0"
-              loading="lazy"
+
+            <Separator
+              orientation="vertical"
+              className="hidden lg:block order-2 h-[70vh] bg-white/40 w-[1px] -ml-4 mt-16"
             />
-          </div>
 
-          <Separator
-            orientation="vertical"
-            className="hidden lg:block order-2 h-[70vh] bg-white/40 w-[1px] -ml-4 mt-16"
-          />
-
-          <div className="order-1 lg:order-3 ml-0 lg:ml-12 space-y-4">
-            <div className="mb-14">
-              <h2 className={cn('text-3xl font-bold', playfair.className)}>
-                Let’s get in touch
-              </h2>
-              <p className="mt-4 text-xs md:text-sm">
-                Or just reach out manually to{' '}
-                <span className="text-white font-medium underline">
-                  thedraperystudioph@gmail.com
-                </span>
-              </p>
+            <div className="order-1 lg:order-3 lg:ml-12 space-y-4 px-6">
+              <div className="mb-14">
+                <h2 className={cn('text-3xl font-bold', playfair.className)}>
+                  Let’s get in touch
+                </h2>
+                <p className="mt-4 text-xs md:text-sm">
+                  Or just reach out manually to{' '}
+                  <span className="text-white font-medium underline">
+                    thedraperystudioph@gmail.com
+                  </span>
+                </p>
+              </div>
+              <form className="space-y-10">
+                <TextField fullWidth variant="standard" label="Name" focused />
+                <TextField
+                  fullWidth
+                  variant="standard"
+                  label="Email"
+                  type="email"
+                  focused
+                />
+                <TextField
+                  fullWidth
+                  variant="standard"
+                  label="Message"
+                  multiline
+                  focused
+                />
+                <Button
+                  type="submit"
+                  variant="outline"
+                  className="w-full lg:w-auto sm:self-start border-white text-white hover:bg-white hover:text-[#633308] transition mx-auto sm:mx-0"
+                >
+                  Send Message
+                </Button>
+              </form>
             </div>
-            <form className="space-y-10">
-              <TextField fullWidth variant="standard" label="Name" focused />
-              <TextField
-                fullWidth
-                variant="standard"
-                label="Email"
-                type="email"
-                focused
-              />
-              <TextField
-                fullWidth
-                variant="standard"
-                label="Message"
-                multiline
-                focused
-                minRows={3}
-              />
-              <Button
-                type="submit"
-                variant="outline"
-                className="w-full lg:w-auto sm:self-start border-white text-white hover:bg-white hover:text-[#633308] transition mx-auto sm:mx-0"
-              >
-                Send Message
-              </Button>
-            </form>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <TransformYourSpace />
+    </>
   );
 }
--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -5,7 +5,7 @@ import MobileNav from './mobile-nav';
 
 export default function Navigation() {
   return (
-    <nav className="bg-gradient-to-b from-black/60 to-transparent z-10 fixed top-0 left-0 right-0">
+    <nav className="bg-gradient-to-b from-black/60 to-transparent z-20 fixed top-0 left-0 right-0">
       <div className="flex items-start max-w-7xl mx-auto p-6 z-20 relative">
         <DraperyLogo withLink className="w-[40px] lg:w-[55px]" />
 
